e5ddf63c09dc625e5039294fe9c80f2f8aa1d911
Rohit19060 <rohitjain19060@gmail.com>
2020-09-17 10:23:50 +0530

    asdf

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "ongodb+srv://new_user:newUser123456@cluster0.rzoa0.mongodb.net/react_express?retryWrites=true&w=majority",
+    "mongodb+srv://new_user:newUser123456@cluster0.rzoa0.mongodb.net/react_express?retryWrites=true&w=majority",
 };
